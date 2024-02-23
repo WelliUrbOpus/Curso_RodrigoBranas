ef933f685e78c9af249ddbec60cd1600a507b874
WelliUrbOpus <wellington.urbano@opuspac.com.br>
2024-02-23 15:02:07 -0300

    Aula Rest API 3
    Essa foi a aula Rest API 3

--- REST_API_2/server/route/postsRoute.js
+++ REST_API_2/server/route/postsRoute.js
@@ -2,23 +2,26 @@ const express = require('express');
 const router = express.Router();
 const postsService = require('../service/postsService');
 
-
-
 router.get('/posts', async function (req, res) {
-    const posts = await postsService.getpost();
+    const posts = await postsService.getposts();
     res.json(posts);
 });
-router.get('/posts/:id', async function (req, res) {
 
-});
 router.post('/posts', async function (req, res) {
-
+    const post = req.body;
+    const newPost = await postsService.savePost(post);
+    res.json(newPost);
 });
+
 router.put('/posts/:id', async function (req, res) {
-
+    const post = req.body;
+    await postsService.updatePost(req.params.id, post);
+    res.end();
 });
-router.delete('posts/:id', async function (req, res) {
 
+router.delete('/posts/:id', async function (req, res) {
+    await postsService.deletePost(req.params.id);
+    res.end();
 });
 
 module.exports = router;
--- REST_API_2/server/service/postsService.js
+++ REST_API_2/server/service/postsService.js
@@ -1,5 +1,21 @@
 const postsData = require('../data/postsData');
 
-exports.getpost = function () {
+exports.getposts = function () {
     return postsData.getPosts();
+};
+
+exports.getpost = function (id) {
+    return postsData.getPost(id);
+};
+
+exports.savePost = function (post) {
+    return postsData.savePosts(post);
+};
+
+exports.deletePost = function (id) {
+    return postsData.deletePost(id);
+};
+
+exports.updatePost = function (id, post) {
+    return postsData.updatePost(id, post);
 };
